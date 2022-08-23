19c7b606fb1f999e5e522f25d358810c232bdf6e
artembilyy <incomecars4u@gmail.com>
2022-08-23 13:59:16 +0300

    added Auto Layout Practice

---
+++ Block 5/Homework Auto Layout raywenderlich/Homework Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework Auto Layout
+//
+//  Created by Артем Билый on 23.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Auto Layout Tutorial in iOS (storyboard)
+    }
+
+
+}
+
